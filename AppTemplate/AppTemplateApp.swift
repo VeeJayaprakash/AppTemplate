63974efb5bc372467f83a36daa2dda816a7a7938
VeeJayaprakash <vijaypescs@gmail.com>
2026-05-19 13:05:58 -0400

    Initial Commit

---
+++ AppTemplate/AppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTemplateApp.swift
+//  AppTemplate
+//
+//  Created by Vijendran  on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct AppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
