079780f42c0d24f5bb3fc0f73e905c86a7ec23b6
VeeJayaprakash <vijaypescs@gmail.com>
2026-05-27 18:27:38 -0400

    Added depenedency container

--- AppTemplate/DependencyContainer.swift
+++ AppTemplate/DependencyContainer.swift
@@ -5,3 +5,37 @@
 //  Created by Vijendran  on 5/19/26.
 //
 
+import Foundation
+import Observation
+
+@MainActor
+@Observable
+final class DependencyContainer {
+    // MARK: - Network Layer
+
+    let networkClient: NetworkClientProtocol
+    var userSession: UserSession
+    let apiClient: APIClient
+
+    // MARK: - Initialization
+
+    init() {
+        // Initialize NetworkClient (base layer)
+        self.networkClient = NetworkClient()
+
+        // Initialize UserSession (token management)
+        let userSession = UserSession(networkClient: networkClient)
+        self.userSession = userSession
+
+        // Initialize APIClient (authenticated wrapper)
+        self.apiClient = APIClient(
+            networkClient: networkClient,
+            tokenProvider: userSession
+        )
+    }
+    
+#if DEBUG
+    static let mockDependencyContainer = DependencyContainer()
+#endif
+}
+
